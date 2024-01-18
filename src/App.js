9b915def1a9d056a20328286fe867020d20d9131
Brandon Cano <brandoncano01@gmail.com>
2024-01-17 23:16:13 -0600

    added file for create account page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Login from './pages/Login.tsx';
+import CreateAccount from './pages/CreateAccount.tsx';
 import Home from './pages/Home.tsx';
 import Profile from './pages/Profile.tsx';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
@@ -10,6 +11,7 @@ const App = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/login" exact element={<Login />} />
+        <Route path="/create_account" exact element={<CreateAccount />} />
         <Route path="/" exact element={<Home />} />
         <Route path="/profile" exact element={<Profile />} />
       </Routes>
---
+++ src/pages/CreateAccount.tsx
@@ -0,0 +1,13 @@
+import "../App.css";
+import React from 'react';
+
+const CreateAccount: React.FC = () => {
+
+    return (
+        <div>
+            <h1>Create Account</h1>
+        </div>
+    )
+}
+
+export default CreateAccount
