d96cf0270079c23db84910b8898acdf33ee34048
Brandon Cano <brandoncano01@gmail.com>
2024-01-17 19:53:10 -0600

    updated app.test.js to new directory

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from '../App';
 
-test('renders learn react link', () => {
+test('renders home page', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Home/i);
   expect(linkElement).toBeInTheDocument();
 });
