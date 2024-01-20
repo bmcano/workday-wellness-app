7da5a128354a02717c99b22ae1607de298034499
Brandon Cano <brandoncano01@gmail.com>
2024-01-20 14:24:19 -0600

    added basic outline for the create account page using material UI

--- src/pages/CreateAccount.tsx
+++ src/pages/CreateAccount.tsx
@@ -1,12 +1,90 @@
 import "../App.css";
 import React from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const defaultTheme = createTheme();
 
 const CreateAccount: React.FC = () => {
 
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        console.log({
+            username: data.get('username'),
+            email: data.get('email'),
+            password: data.get('password'),
+            confirm_password: data.get('confirm_password')
+        });
+    };
+
     return (
-        <div>
-            <h1>Create Account</h1>
-        </div>
+        <React.Fragment>
+             <ThemeProvider theme={defaultTheme}>
+                <Container component="main" maxWidth="xs">
+                    <Box sx={{
+                        marginTop: 8,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}>
+                        <Typography component="h1" variant="h5">Create Account</Typography>
+                        <Box component="form" onSubmit={handleSubmit} noValidate >
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="username"
+                                label="Username"
+                                name="username"
+                                autoComplete="username"
+                                autoFocus />
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="email"
+                                label="Email Address"
+                                name="email"
+                                autoComplete="email"
+                                autoFocus />
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                name="password"
+                                label="Password"
+                                type="password"
+                                id="password"
+                                autoComplete="current-password" />
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                name="confirm_password"
+                                label="Confirm Password"
+                                type="confirm_password"
+                                id="confirm_password"
+                                autoComplete="confirm-current-password" />
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                sx={{ mt: 2, mb: 2 }}
+                            >
+                                Create Account
+                            </Button>
+                        </Box>
+                    </Box>
+                </Container>
+            </ThemeProvider>
+        </React.Fragment>
     )
 }
 
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,12 +1,10 @@
 import "../App.css";
-import React, { useState } from 'react';
-import Avatar from '@mui/material/Avatar';
+import React from 'react';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
