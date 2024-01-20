c900f093a6d0ff35203efa54ff5a55d7924e3185
Brandon Cano <brandoncano01@gmail.com>
2024-01-20 14:08:39 -0600

    added in material UI and made a login screen, not functional yet

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,31 +1,79 @@
 import "../App.css";
 import React, { useState } from 'react';
-import TextBox from "../components/TextBox.tsx";
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const defaultTheme = createTheme();
 
 const Login: React.FC = () => {
 
-    const [inputEmail, setInputEmail] = useState<string>('');
-    const [inputPassword, setInputPassword] = useState<string>('');
-
-    const handleEmailChange = (newValue: string) => {
-        setInputEmail(newValue);
-    };
-
-    const handlePasswordChange = (newValue: string) => {
-        setInputPassword(newValue);
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        console.log({
+          email: data.get('email'),
+          password: data.get('password'),
+        });
     };
 
     return (
         <React.Fragment>
-            <h1>Login</h1>
-            <TextBox
-                value={inputEmail}
-                onChange={handleEmailChange}
-                placeholder="Email" />
-            <TextBox
-                value={inputPassword}
-                onChange={handlePasswordChange}
-                placeholder="Passowrd" />
+             <ThemeProvider theme={defaultTheme}>
+                <Container component="main" maxWidth="xs">
+                    <Box sx={{
+                        marginTop: 8,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}>
+                        <Typography component="h1" variant="h5">Login</Typography>
+                        <Box component="form" onSubmit={handleSubmit} noValidate >
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="email"
+                                label="Email Address"
+                                name="email"
+                                autoComplete="email"
+                                autoFocus />
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                name="password"
+                                label="Password"
+                                type="password"
+                                id="password"
+                                autoComplete="current-password" />
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                sx={{ mt: 2, mb: 2 }}
+                            >
+                                Login
+                            </Button>
+                            <Grid container>
+                                <Grid item xs>
+                                    <Link href="" variant="body2">Forgot password?</Link>
+                                </Grid>
+                                <Grid item>
+                                    <Link href="/create_account" variant="body2">Don't have an account?</Link>
+                                </Grid>
+                            </Grid>
+                        </Box>
+                    </Box>
+                </Container>
+            </ThemeProvider>
         </React.Fragment>
     )
 }
