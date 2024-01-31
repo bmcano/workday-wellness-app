b38dcf793ccdaf6978ec50a508a5486a67dd01f9
Brandon Cano <brandoncano01@gmail.com>
2024-01-31 17:42:59 -0600

    #2 - initial update of schema and seeding

--- db/models/Users.js
+++ db/models/Users.js
@@ -2,10 +2,11 @@ import { Schema, model } from 'mongoose';
 
 const UserSchema = new Schema({
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true, unique: false },
-    first_name: { type: String, required: true, unique: false },
-    last_name: { type: String, required: true, unique: false },
-    stub_data: { type: Boolean, required: false, unique: false }
+    password: { type: String, required: true },
+    first_name: { type: String, required: true },
+    last_name: { type: String, required: true },
+    stub_data: { type: Boolean, required: false },
+    friends: { type: [String], default: [] }
 });
 
 const UserModel = model("users", UserSchema);
