5f2a4bd00cf2ddf4852320e16bdafc05d4f713d9
Brandon Cano <brandoncano01@gmail.com>
2024-01-31 19:14:34 -0600

    #2 - centered the header and added the stylings for a list of items on the white screen

--- src/pages/Friends.tsx
+++ src/pages/Friends.tsx
@@ -5,6 +5,7 @@ import { AuthorizedUser } from "../api/AuthorizedUser.tsx";
 import { useNavigate } from "react-router-dom";
 import { Friend } from "../context/Friend.tsx";
 import Button from '@mui/material/Button';
+import Typography from "@mui/material/Typography";
 
 const Friends: React.FC = () => {
 
@@ -30,17 +31,19 @@ const Friends: React.FC = () => {
     return (
         <React.Fragment>
             <Navbar />
-            <h1>Friends</h1>
-            <ul className="friends-list">
-                {friendsList.map((friend, index) => (
-                    <li key={index} className="friend-item">
-                        <div className="friend-name">{`${friend.first_name} ${friend.last_name}`}</div>
-                        <Button variant="contained" color="primary" onClick={() => navigate(`/profile/${friend.id}`)}>
-                            View Profile
-                        </Button>
-                    </li>
-                ))}
-            </ul>
+            <Typography component="h1" variant="h4" align="center" marginTop={4}>Friends</Typography>
+            <div className="card">
+                <ul className="card-list">
+                    {friendsList.map((friend, index) => (
+                        <><li key={index} className="card-item">
+                            <div className="card-text">{`${friend.first_name} ${friend.last_name}`}</div>
+                            <Button variant="contained" color="primary" onClick={() => navigate(`/profile/${friend.id}`)}>
+                                View Profile
+                            </Button>
+                        </li>{index < friendsList.length - 1 && <div className="divider"></div>}</>
+                    ))}
+                </ul>
+            </div>
         </React.Fragment>
     )
 }
