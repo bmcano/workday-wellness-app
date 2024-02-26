432093096f179b7d00fd9b5071383448e4335cc9
AzulitePoke <71411978+AlexErrand@users.noreply.github.com>
2024-02-26 17:25:57 -0600

    Added socials to pfp and fixed formatting

--- src/pages/FAQ.tsx
+++ src/pages/FAQ.tsx
@@ -3,7 +3,18 @@ import { useNavigate } from "react-router-dom";
 import Navbar from "../components/Navbar.tsx";
 // @ts-ignore
 import pfpImage from '../static/images/default_profile_picture.png';
-import pfpImage2 from 'src/static/images/NateGriffin.png';
+// @ts-ignore
+import alexpfp from '../static/images/alexpfp.jpeg';
+// @ts-ignore
+import rogpfp from '../static/images/rogeliopfp.jpeg';
+// @ts-ignore
+import brandonpfp from '../static/images/brandonpfp.jpeg';
+// @ts-ignore
+import ianpfp from '../static/images/default_profile_picture.png';
+// @ts-ignore
+import linkedinicon from '../static/images/linkedin image.png';
+// @ts-ignore
+import githubicon from '../static/images/github image.png';
 import { AuthorizedUser } from "../api/AuthorizedUser.tsx";
 import "../App.css";
 
@@ -25,13 +36,64 @@ const Profile: React.FC = () => {
   const renderProfilePic = () => {
     switch (activeTab) {
       case 'Alex':
-        return <img src={pfpImage} alt="Profile" />;
+        return <img src={alexpfp} alt="Profile" />;
       case 'Brandon':
-        return <img src={pfpImage2} alt="Profile" />;
+        return <img src={brandonpfp} alt="Profile" />;
       case 'Ian':
-        return <img src={pfpImage} alt="Profile" />;
+        return <img src={ianpfp} alt="Profile" />;
       case 'Rogelio':
-        return <img src={pfpImage} alt="Profile" />;
+        return <img src={rogpfp} alt="Profile" />;
+      default:
+        <img src={pfpImage} alt="Profile" />;
+    }
+  };
+
+  const renderSocials = () => {
+    switch (activeTab) {
+      case 'Alex':
+        return (
+          <div className="social-links-container">
+            <a href={"https://github.com/AlexErrand"} target="_blank" rel="noopener noreferrer">
+              <img src={githubicon} alt="GitHub Profile" className="social-icon"/>
+            </a>
+            <a href={"https://www.linkedin.com/in/michael-arand/"} target="_blank" rel="noopener noreferrer">
+              <img src={linkedinicon} alt="LinkedIn Profile" className="social-icon" />
+            </a>
+          </div>
+        );
+      case 'Brandon':
+        return (
+          <div className="social-links-container">
+            <a href={"https://github.com/bmcano"} target="_blank" rel="noopener noreferrer">
+              <img src={githubicon} alt="GitHub Profile" className="social-icon"/>
+            </a>
+            <a href={"https://www.linkedin.com/in/brandon-cano/"} target="_blank" rel="noopener noreferrer">
+              <img src={linkedinicon} alt="LinkedIn Profile" className="social-icon"/>
+            </a>
+          </div>
+        );
+      case 'Ian':
+        return (
+          <div className="social-links-container">
+            <a href={"https://github.com/IanKuk"} target="_blank" rel="noopener noreferrer">
+              <img src={githubicon} alt="GitHub Profile" className="social-icon"/>
+            </a>
+            <a href={""} target="_blank" rel="noopener noreferrer">
+              <img src={linkedinicon} alt="LinkedIn Profile" className="social-icon"/>
+            </a>
+          </div>
+        );
+      case 'Rogelio':
+        return (
+          <div className="social-links-container">
+            <a href={"https://github.com/RedJelloooo"} target="_blank" rel="noopener noreferrer">
+              <img src={githubicon} alt="GitHub Profile" className="social-icon"/>
+            </a>
+            <a href={"https://www.linkedin.com/in/rvalleia/"} target="_blank" rel="noopener noreferrer">
+              <img src={linkedinicon} alt="LinkedIn Profile" className="social-icon"/>
+            </a>
+          </div>
+        );
       default:
         <img src={pfpImage} alt="Profile" />;
     }
@@ -40,34 +102,43 @@ const Profile: React.FC = () => {
   const renderBasicInfo = () => {
     switch (activeTab) {
       case 'Alex':
-        return <div>About content goes here.</div>;
+        return (
+          <>
+            <h1>Alex</h1>
+            <p>{'Front End'}</p>
+            <p>Since January 2024</p>
+          </>
+        );
       case 'Brandon':
-        return <div>Latest Activity content goes here.</div>;
+        return (
+          <>
+            <h1>Brandon</h1>
+            <p>{'{Insert Role at Company}'}</p>
+            <p>Since Jan 1st 1999</p>
+          </>
+        );
       case 'Ian':
-        return <div>Posts content goes here.</div>;
+        return (
+          <>
+            <h1>Ian</h1>
+            <p>{'{Insert Role at Company}'}</p>
+            <p>Since Jan 1st 1999</p>
+          </>
+        );
       case 'Rogelio':
-        return <div>Status content goes here.</div>;
+        return (
+          <>
+            <h1>Rogelio</h1>
+            <p>{'{Insert Role at Company}'}</p>
+            <p>Since Jan 1st 1999</p>
+          </>
+        );
       default:
         return <div>Select a tab.</div>;
     }
   };
 
- 
 
-  const renderTabContent = () => {
-    switch (activeTab) {
-      case 'Alex':
-        return <div>About content goes here.</div>;
-      case 'Brandon':
-        return <div>Latest Activity content goes here.</div>;
-      case 'Ian':
-        return <div>Posts content goes here.</div>;
-      case 'Rogelio':
-        return <div>Status content goes here.</div>;
-      default:
-        return <div>Select a tab.</div>;
-    }
-  };
 
   return (
     <React.Fragment>
@@ -75,16 +146,7 @@ const Profile: React.FC = () => {
       <div className="card-columns">
         <div className="card-column">
           <div className="card card-span-4">
-            <div className="profile-content-container">
-              <div className="profile-picture-page" onClick={() => navigate("/profile/edit")}>
-                {renderProfilePic()}
-              </div>
-              <div className="profile-text-container">
-                <h1>Ian</h1>
-                <p>{'{Insert Role at Company}'}</p>
-                <p>Since Jan 1st 1999</p>
-              </div>
-            </div>
+          <h1>Meet the developers!</h1>
             <div className="card-header">
               {TABS.map((tab) => (
                 <button
@@ -97,8 +159,27 @@ const Profile: React.FC = () => {
               ))}
             </div>
             <div className="divider" />
+            <div className="profile-content-container">
+              
+              <div className="profile-picture-page" onClick={() => navigate("/profile/edit")}>
+                {renderProfilePic()}
+              </div>
+              <div className="profile-text-container">
+                {renderBasicInfo()}
+              </div>
+              <div className="profile-text-container">
+                {renderSocials()}
+              </div>
+            </div>
             <div className="card-content">
-              {renderTabContent()}
+              <p>This project was create for our senior design project at the University of Iowa. The original proposal and project is being sponsored by State Farm.
+
+                  add description of what app does
+
+                  add faqs as we think of them
+
+                  disclaimer about how we are not medical professionals and that the recommendations are by our best guesses
+              </p>
             </div>
           </div>
         </div>
