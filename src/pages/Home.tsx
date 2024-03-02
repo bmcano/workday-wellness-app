6d89a94608f8d68e77a102d260f978886ebe1d37
RedJelloooo <rogelio-valle@uiowa.edu>
2024-03-02 12:02:00 -0600

    #36 chat bot now opens in right side drawer

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,13 +11,7 @@ import { getCurrentFormattedDate } from "../util/dateUtils.ts";
 import { apiGet } from "../api/serverApiCalls.tsx";
 import UpcomingEvents from "../components/UpcomingEvents.tsx";
 import { EventInput } from "@fullcalendar/core";
-//testing
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
-
+import Drawer from '@mui/material/Drawer';
 let intervalId: number | null = null;
 
 const Home: React.FC = () => {
@@ -48,11 +42,11 @@ const Home: React.FC = () => {
     };
   }, [navigate]);
 
-  const handleClickOpen = () => {
+  //drawers for AI chat bot
+  const handleDrawerOpen = () => {
     setOpen(true);
   };
-
-  const handleClose = () => {
+  const handleDrawerClose = () => {
     setOpen(false);
   };
   
@@ -169,24 +163,19 @@ const Home: React.FC = () => {
           <div>
             <UpcomingEvents events={todaysEvent} />
           </div>
-          <Button variant="outlined" color="primary" onClick={handleClickOpen}>
-            Chat with the Workday Wellness Assistant!
+          <Button variant="outlined" color="primary" onClick={handleDrawerOpen}>
+            Talk with the Workday Wellness Assistant!
           </Button>
-          <Dialog open={open} onClose={handleClose} maxWidth="md" fullWidth>
-            <DialogTitle>{"Workday Wellness Assistant"}</DialogTitle>
-            <DialogContent>
-              <div
-                dangerouslySetInnerHTML={{
-                  __html: `<iframe id="UTUFTPWDEN" loading="eager" src="https://embed.pickaxeproject.com/axe?id=Workday_Wellness_Assistant_LGKZX&mode=embed_gold&host=beta&theme=custom&opacity=100&font_header=Real+Head+Pro&size_header=30&font_body=Real+Head+Pro&size_body=16&font_labels=Real+Head+Pro&size_labels=14&font_button=Real+Head+Pro&size_button=16&c_fb=&c_ff=F01716&c_fbd=F01716&c_bb=F01716&c_bt=FFFFFF&c_t=000000&s_ffo=100&s_bbo=100&s_f=minimalist&s_b=filled&s_t=2&s_to=1&s_r=2" width="100%" height="400px" onMouseOver="this.style.boxShadow='0px 6px 6px -3px rgba(0,0,0,0.1)'" onMouseOut="this.style.boxShadow='none'" style="border:1px solid rgba(0, 0, 0, 1);transition:.3s;border-radius:4px;" frameBorder="0"></iframe>`
-                }}
-              ></div>
-            </DialogContent>
-            <DialogActions>
-              <Button onClick={handleClose} color="primary">
-                Close
-              </Button>
-            </DialogActions>
-          </Dialog>
+          <Drawer anchor="right" open={open} onClose={handleDrawerClose}>
+           <div
+              dangerouslySetInnerHTML={{
+                __html: `<iframe id="UTUFTPWDEN" loading="eager" src="https://embed.pickaxeproject.com/axe?id=Workday_Wellness_Assistant_LGKZX&mode=embed_gold&host=beta&theme=custom&opacity=100&font_header=Real+Head+Pro&size_header=30&font_body=Real+Head+Pro&size_body=16&font_labels=Real+Head+Pro&size_labels=14&font_button=Real+Head+Pro&size_button=16&c_fb=&c_ff=F01716&c_fbd=F01716&c_bb=F01716&c_bt=FFFFFF&c_t=000000&s_ffo=100&s_bbo=100&s_f=minimalist&s_b=filled&s_t=2&s_to=1&s_r=2" width="100%" height="850px" onMouseOver="this.style.boxShadow='0px 6px 6px -3px rgba(0,0,0,0.1)'" onMouseOut="this.style.boxShadow='none'" style="border:1px solid rgba(0, 0, 0, 1);transition:.3s;border-radius:4px;" frameBorder="0"></iframe>`
+              }}
+            ></div>
+            <Button onClick={handleDrawerClose} color="primary">
+              Close
+            </Button>
+          </Drawer>
         </div>
       </div>
     </React.Fragment>
