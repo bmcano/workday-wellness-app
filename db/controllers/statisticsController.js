f4b176e47156490aaec7f8ed41932a1a21187ee5
Brandon Cano <brandoncano01@gmail.com>
2024-03-21 16:21:06 -0500

    #50 - added global leaderboard server calls

--- db/controllers/statisticsController.js
+++ db/controllers/statisticsController.js
@@ -1,5 +1,5 @@
-import StatisticsModel from "../models/Statistics";
-import { getUserInformation } from "./sessionController";
+import StatisticsModel from "../models/Statistics.js";
+import { getUserInformation } from "./sessionController.js";
 
 /**
  * GET:
@@ -17,8 +17,8 @@ export const getGlobalLeaderboardStreak = async (req, res) => {
         const data = getUserInformation(token);
         if (data) {
             const user = await StatisticsModel.findOne({ email: data.email }).lean();
-            const users = await StatisticsModel.find({}).lean();
-            const leaderboard = users.filter(); // will want to have placement and name(?) in each item
+            const users = await StatisticsModel.find({}).sort({ streak: -1 }).lean();
+            const leaderboard = users.map((users, index) => ({ ...users, placement: index + 1 }));
             return res.json({ authorized: true, leaderboard: leaderboard, user: user });
         } else {
             return res.json({ authorized: false });
@@ -30,7 +30,21 @@ export const getGlobalLeaderboardStreak = async (req, res) => {
 }
 
 export const getGlobalLeaderboardCompleted = async (req, res) => {
-
+    try {
+        const token = req.headers.authorization.split(' ')[1];
+        const data = getUserInformation(token);
+        if (data) {
+            const user = await StatisticsModel.findOne({ email: data.email }).lean();
+            const users = await StatisticsModel.find({}).sort({ "completed.amount": -1 }).lean();
+            const leaderboard = users.map((users, index) => ({ ...users, placement: index + 1 }));
+            return res.json({ authorized: true, leaderboard: leaderboard, user: user });
+        } else {
+            return res.json({ authorized: false });
+        }
+    } catch (error) {
+        console.log(error);
+        return res.json({ authorized: false });
+    }
 }
 
 export const getFriendLeaderboardStreak = async (req, res) => {
