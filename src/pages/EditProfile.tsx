f7fdf7bfec1689e4ac9711d2b6d04b0e8d0e35de
Brandon Cano <brandoncano01@gmail.com>
2024-03-27 14:02:22 -0500

    #65 - updated edit profile page, items from DB show in textfields

--- src/pages/EditProfile.tsx
+++ src/pages/EditProfile.tsx
@@ -4,16 +4,20 @@ import { AuthorizedUser } from "../api/AuthorizedUser.tsx";
 import { useNavigate } from "react-router-dom";
 import UploadImage from "../components/UploadImage.tsx";
 import imageCompression from "browser-image-compression";
-import { apiPost } from "../api/serverApiCalls.tsx";
+import { apiGet, apiPost } from "../api/serverApiCalls.tsx";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Navbar from "../components/Navbar.tsx";
 import Card from "../components/card/Card.tsx";
 import Column from "../components/card/Column.tsx";
+import CardText from "../components/card/CardText.tsx"
+import Divider from "../components/card/Divider.tsx";
 
 const EditProfile: React.FC = () => {
     const navigate = useNavigate();
+    const [isInfoLoading, setIsInfoLoading] = useState(true);
+    const [isScheduleLoading, setIsScheduleLoading] = useState(true);
     const [firstName, setFirstName] = useState<string>('');
     const [lastName, setLastName] = useState('');
     const [birthday, setBirthday] = useState('');
@@ -29,6 +33,39 @@ const EditProfile: React.FC = () => {
 
     useEffect(() => {
         AuthorizedUser(navigate);
+        apiGet("/user")
+            .then(data => {
+                if (data.authorized) {
+                    setFirstName(data.user.first_name);
+                    setLastName(data.user.last_name);
+                    if (data.user.birthday) {
+                        setBirthday(data.user.birthday.slice(0, 10));
+                    }
+                    setAbout(data.user.about);
+                    setLinkedinLink(data.user.linkedIn_link);
+                }
+            })
+            .catch(error => console.log(error))
+            .finally(() => setIsInfoLoading(false));
+
+        apiGet("/schedule")
+            .then(data => {
+                if (data.authorized) {
+                    const schedule = data.schedule;
+                    setWorkHours({
+                        Monday: { start: schedule.monday_start, end: schedule.monday_end },
+                        Tuesday: { start: schedule.tuesday_start, end: schedule.tuesday_end },
+                        Wednesday: { start: schedule.wednesday_start, end: schedule.wednesday_end },
+                        Thursday: { start: schedule.thursday_start, end: schedule.thursday_end },
+                        Friday: { start: schedule.friday_start, end: schedule.friday_end }
+                    })
+                } else {
+                    console.log("Using default scheudle.");
+                }
+            })
+            .catch(error => console.log(error))
+            .finally(() => setIsScheduleLoading(false));
+
     }, [navigate]);
 
     const handleImageUpload = async (image: File) => {
@@ -103,28 +140,28 @@ const EditProfile: React.FC = () => {
             <Navbar />
             <Column>
                 <div>
-                    <Card>
-                        <h3>Image Upload</h3>
-                        <UploadImage handleImageUpload={handleImageUpload} />
-                    </Card>
+                    <UploadImage handleImageUpload={handleImageUpload} />
                     <form onSubmit={handleSubmit}>
                         <Card>
-                            <h3>Profile Information</h3>
-                            <TextField label="First Name" value={firstName} onChange={e => setFirstName(e.target.value)} fullWidth margin="normal" />
-                            <TextField label="Last Name" value={lastName} onChange={e => setLastName(e.target.value)} fullWidth margin="normal" />
-                            <TextField label="Birthday" type="date" value={birthday} onChange={e => setBirthday(e.target.value)} InputLabelProps={{ shrink: true }} fullWidth margin="normal" />
-                            <TextField label="About" multiline rows={4} value={about} onChange={e => setAbout(e.target.value)} fullWidth margin="normal" />
-                            <TextField label="LinkedIn Link" value={linkedinLink} onChange={e => setLinkedinLink(e.target.value)} fullWidth margin="normal" />
-                            <Button type="submit" variant="contained" color="primary" style={{ marginTop: '20px' }}>Update Profile</Button>
+                            <CardText type="header" text="Profile Information" style={{ marginTop: "0px", marginBottom: "0px" }} />
+                            <Divider />
+                            {!isInfoLoading && <div>
+                                <TextField label="First Name" value={firstName} onChange={e => setFirstName(e.target.value)} fullWidth margin="normal" />
+                                <TextField label="Last Name" value={lastName} onChange={e => setLastName(e.target.value)} fullWidth margin="normal" />
+                                <TextField label="Birthday" type="date" value={birthday} onChange={e => setBirthday(e.target.value)} InputLabelProps={{ shrink: true }} fullWidth margin="normal" />
+                                <TextField label="About" multiline rows={4} value={about} onChange={e => setAbout(e.target.value)} fullWidth margin="normal" />
+                                <TextField label="LinkedIn Link" value={linkedinLink} onChange={e => setLinkedinLink(e.target.value)} fullWidth margin="normal" />
+                                <Button type="submit" variant="contained" color="primary" style={{ marginTop: '20px' }}>Update Profile</Button></div>}
                         </Card>
                     </form>
                 </div>
                 <form onSubmit={handleSchedule}>
                     <Card>
-                        <h3>Work Schedule</h3>
-                        {Object.keys(workHours).map((day) => (
+                        <CardText type="header" text="Work Schedule" style={{ marginTop: "0px", marginBottom: "0px" }} />
+                        <Divider />
+                        {!isScheduleLoading && Object.keys(workHours).map((day) => (
                             <div key={day}>
-                                <Typography variant="body1" style={{ marginLeft: '32px', marginTop: '16px' }}>{day}</Typography>
+                                <CardText type="title" text={day} style={{ marginLeft: "16px", marginBottom: "8px"}}/>
                                 <TextField
                                     label="Start Time"
                                     type="time"
@@ -132,7 +169,7 @@ const EditProfile: React.FC = () => {
                                     onChange={(e) => handleWorkHoursChange(day, 'start', e.target.value)}
                                     InputLabelProps={{ shrink: true }}
                                     margin="normal"
-                                    style={{ marginLeft: '32px' }}
+                                    style={{ marginLeft: '16px' }}
                                 />
                                 <TextField
                                     label="End Time"
@@ -141,11 +178,11 @@ const EditProfile: React.FC = () => {
                                     onChange={(e) => handleWorkHoursChange(day, 'end', e.target.value)}
                                     InputLabelProps={{ shrink: true }}
                                     margin="normal"
-                                    style={{ marginLeft: '32px', marginRight: "32px" }}
+                                    style={{ marginLeft: '16px', marginRight: "32px" }}
                                 />
                             </div>
                         ))}
-                        <Button type="submit" variant="contained" color="primary" style={{ marginLeft: '32px', marginTop: '16px' }}>Update Schedule</Button>
+                        <Button type="submit" variant="contained" color="primary" style={{ marginLeft: '16px', marginTop: '16px' }}>Update Schedule</Button>
                     </Card>
                 </form>
             </Column>
