c5fb8a3830413495d2deed55311f963ab98b13f7
RedJelloooo <rogelio-valle@uiowa.edu>
2024-04-04 20:33:24 -0500

    #68 added email to privacy schema

--- db/models/Privacy.js
+++ db/models/Privacy.js
@@ -1,6 +1,7 @@
 import { Schema, model } from 'mongoose';
 
 const PrivacySchema = new Schema({
+    email: { type: String, required: true, unique: true },
     publicProfile: { type: Boolean, default: true },
     birthdayPrivate: { type: Boolean, default: true },
     aboutPrivate: { type: Boolean, default: true },
