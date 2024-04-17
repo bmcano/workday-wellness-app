f4e102175f6447f2c836f912236be66264b515d4
Brandon Cano <brandoncano01@gmail.com>
2024-04-17 12:32:02 -0500

    #40 - added featureflags so that we can block on items for production and live demos

---
+++ src/FeatureFlags.ts
@@ -0,0 +1,5 @@
+// This file contains flags so we can easily disable features we do not want to have appear in production or other demo enviornments
+export default class FeatureFlags {
+    ChatBotEnabled = false;
+    LifetimeStatsEnabled = false;
+}
